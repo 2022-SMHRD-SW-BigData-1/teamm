07c0826a38332937cbdab405fcf8d9ff2949f73a
SMHRD <SMHRD@DESKTOP-H3UDQS4>
2022-08-31 12:02:06 +0900

    곱하기

---
+++ teammpro/src/teammpro/ljg.java
@@ -0,0 +1,23 @@
+package teammpro;
+
+import java.util.Scanner;
+
+public class ljg {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("A 입력 : ");
+		int num = sc.nextInt();
+		
+		System.out.println("B 입력 : ");
+		int num1 = sc.nextInt();
+		
+		int result = num*num1;
+		
+		System.out.println(result);
+		
+	}
+
+}
