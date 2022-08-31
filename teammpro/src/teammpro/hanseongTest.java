7aa6a0942beff34445e0cc963fcc223a8b0f0c1f
SMHRD <SMHRD@DESKTOP-VKIJMEU>
2022-08-31 11:38:54 +0900

    커밋이 안되는거 같은데 기분탓인가?

--- teammpro/src/teammpro/hanseongTest.java
+++ teammpro/src/teammpro/hanseongTest.java
@@ -6,6 +6,7 @@ public class hanseongTest {
 		// TODO Auto-generated method stub
 		System.out.println("나는야 퉁퉁이 골목 대장이라네~~~~~~~~~~~~~");
 		System.out.println("야야야야야ㅑ야야야야야양~~~~~~~~~~~~~~~");
+		System.out.println("푸쉬가 안되는데?");
 	}
 
 }
