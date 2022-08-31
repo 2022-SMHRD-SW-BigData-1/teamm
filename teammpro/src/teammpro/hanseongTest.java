bf1f7986d58689cf31fd35bab303a913275b7829
SMHRD <SMHRD@DESKTOP-VKIJMEU>
2022-08-31 11:35:18 +0900

    한성이 새로운 클래스 만듬

---
+++ teammpro/src/teammpro/hanseongTest.java
@@ -0,0 +1,11 @@
+package teammpro;
+
+public class hanseongTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("나는야 퉁퉁이 골목 대장이라네~~~~~~~~~~~~~");
+		System.out.println("야야야야야ㅑ야야야야야양~~~~~~~~~~~~~~~");
+	}
+
+}
