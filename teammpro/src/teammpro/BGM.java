a55eb05bb2ddfda3dbd6a850795e4a9c1d5b1d58
smhrd <smhrd@DESKTOP-T55GTEU>
2022-09-05 09:55:32 +0900

    exe파일을 만들기 위한 음악 수정

--- teammpro/src/teammpro/BGM.java
+++ teammpro/src/teammpro/BGM.java
@@ -8,19 +8,19 @@ public class BGM {
 	ArrayList<MusicVO> playList = new ArrayList<MusicVO>();
 	
 	public BGM() {
-		playList.add(new MusicVO("첫화면", "src//music//첫화면.mp3"));
-		playList.add(new MusicVO("함성박수1", "src//music//함성박수1.mp3"));
-		playList.add(new MusicVO("반갑습니다 에이치", "src//music//반갑습니다 에이치.mp3"));
-		playList.add(new MusicVO("스포츠_타이틀", "src//music//스포츠_타이틀.mp3"));
-		playList.add(new MusicVO("1등등장", "src//music//1등등장.mp3"));
-		playList.add(new MusicVO("말 달리는 소리2", "src//music//말 달리는 소리2.mp3"));
-		playList.add(new MusicVO("말 등장", "src//music//말 등장.mp3"));
-		playList.add(new MusicVO("빵빠레", "src//music//빵빠레.mp3"));
-		playList.add(new MusicVO("선수소개", "src//music//선수소개.mp3"));
-		playList.add(new MusicVO("소리 내면서 달리는 말", "src//music//소리 내면서 달리는 말.mp3"));
-		playList.add(new MusicVO("환호하는관중", "src//music//환호하는관중.mp3"));
-		playList.add(new MusicVO("휘파람박수", "src//music//휘파람박수.mp3"));
-		playList.add(new MusicVO("말 소개", "src//music//말 소개.mp3"));
+		playList.add(new MusicVO("첫화면", "music//첫화면.mp3"));
+		playList.add(new MusicVO("함성박수1", "music//함성박수1.mp3"));
+		playList.add(new MusicVO("반갑습니다 에이치", "music//반갑습니다 에이치.mp3"));
+		playList.add(new MusicVO("스포츠_타이틀", "music//스포츠_타이틀.mp3"));
+		playList.add(new MusicVO("1등등장", "music//1등등장.mp3"));
+		playList.add(new MusicVO("말 달리는 소리2", "music//말 달리는 소리2.mp3"));
+		playList.add(new MusicVO("말 등장", "music//말 등장.mp3"));
+		playList.add(new MusicVO("빵빠레", "music//빵빠레.mp3"));
+		playList.add(new MusicVO("선수소개", "music//선수소개.mp3"));
+		playList.add(new MusicVO("소리 내면서 달리는 말", "music//소리 내면서 달리는 말.mp3"));
+		playList.add(new MusicVO("환호하는관중", "music//환호하는관중.mp3"));
+		playList.add(new MusicVO("휘파람박수", "music//휘파람박수.mp3"));
+		playList.add(new MusicVO("말 소개", "music//말 소개.mp3"));
 	}
 	
 	public MusicVO p1() {
